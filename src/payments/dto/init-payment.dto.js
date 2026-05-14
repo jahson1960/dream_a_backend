d9c3d8f88370c2f9ff695e42b0488532bbc119e9
Victor Oguanobi <jahson1960@gmail.com>
2026-05-14 14:08:51 +0100

    retrieve js

--- src/payments/dto/init-payment.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InitPaymentDto = void 0;
-var InitPaymentDto = /** @class */ (function () {
-    function InitPaymentDto() {
-    }
-    return InitPaymentDto;
-}());
-exports.InitPaymentDto = InitPaymentDto;
